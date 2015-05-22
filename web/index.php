f3e05302e5b130f0c23c30e76cf91b1e35b22fb1
santicordoba <sdcsantiago@gmail.com>
2015-05-22 15:41:00 -0300

    Revert "Revert "web""
    This reverts commit 3ab72594495a869324ac09ace5b12b30c1e46632.

---
+++ web/index.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>BESTNID: Inicio</title>
+		<link type="text/css" id="global-css" rel="stylesheet" href="css/header.css" media="all">
+	</head>
+
+
+	<body>
+		<div class="wrapper">
+			<div class="pagina">
+				<div class="login">
+					<img src="imagenes/logo2.png" height="45">
+					<a href="/bestnid/registrate.php">REGISTRATE GRATIS</a>
+					<form name="login" method="post" action="modulos/login.php">
+						E-mail <input type="text" name="email" value="@">
+						Contrase&ntilde;a <input type="password" name="pass">
+						<input type="submit" value="Ingresar">
+					</form>
+				</div>
+			</div>	
+			<div class="footer">
+				Copyright &copy; 2015 - Desarrollo Grupo MAS
+			</div>
+		</div>
+	</body>	
+</html>
