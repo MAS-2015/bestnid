3100240109b12c56720babc8ed4f9560f6227237
santicordoba <sdcsantiago@gmail.com>
2015-05-22 20:03:01 -0300

    web
    archivos de la web en desarrollo

---
+++ web/registrate.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<title>BESTNID: Inicio</title>
+		<link type="text/css" id="global-css" rel="stylesheet" href="css/header.css" media="all">
+		<link type="text/css" id="global-css" rel="stylesheet" href="css/registro.css" media="all">
+	</head>
+
+
+	<body>
+		<div class="wrapper">
+			<div class="pagina">
+				<div class="login">
+				<img src="imagenes/logo2.png" height="45">
+
+				<form>
+					E-mail <input type="text" name="email" value="@">
+					Contrase&ntilde;a <input type="password" name="pass">
+					<input type="submit" value="Ingresar">
+				</form>
+				</div>
+				<div class="registro">
+
+						<form>
+							E-mail: <input type="text" name="email" value="@"><span class="oblig">*</span><br><br>
+							Contrase&ntilde;a: <input type="password" name="pass"><span class="oblig">*</span><br><br>
+							Nombre y Apellido: <input type="text" name="nomyApe"><span class="oblig">*</span><br><br>
+							Numero de Tarjeta: <input type="number" name="nroTarj"><span class="oblig">*</span><br><br>
+							Numero de Telefono: <input type="number" name="nroTel"><br><br>
+							Domicilio: <input type="text" name="domicilio"><br><br>
+
+						</form>
+				</div>
+			</div>	
+
+			<div class="footer">
+				Copyright &copy; 2015 - Desarrollo Grupo MAS
+			</div>
+		</div>	
+	</body>
+
+</html>
