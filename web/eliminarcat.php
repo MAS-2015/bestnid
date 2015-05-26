fb41e5b10929a94d836e262cc0ecee6f692da08d
Mauroech <shukaku-5kaku@hotmail.com>
2015-05-26 11:34:50 -0300

    eliminar categoria

---
+++ web/eliminarcat.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<link type="text/css" id="global-css" rel="stylesheet" href="css/registro.css" media="all">
+		<link type="text/css" id="global-css" rel="stylesheet" href="css/header.css" media="all">
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="registro">
+				<p class="infoR">Seleccionar categoria a eliminar</p>
+				<form method="post" action="eliminarcat.php">
+					<?php 
+					include("modulos/eli_categoria.php");
+					include("modulos/selector_categorias.php");
+					?>
+					<input type="submit"  value="Eliminar" name="boton"/><br><br>
+					<?php
+						if(isset($salida)){
+							echo "$salida";
+	}
+					?>
+			</form>
+			</div>
+		</div>
+		<div class="footer">
+				Copyright &copy; 2015 - Desarrollo Grupo MAS
+		</div>
+
+	</body>
+</html>
