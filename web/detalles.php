0b0bd9c656abcf1fce94f839c3dd6a426580f858
Agustín <agustinlizarralde@gmail.com>
2015-05-31 15:59:09 -0300

    Agregado vista en detalle

---
+++ web/detalles.php
@@ -0,0 +1,57 @@
+<html>
+	<head>
+	<?php
+	include("/modulos/conexion.php");
+	include("/modulos/funciones_detalle.php");
+	
+	$tituloSubasta="";
+	$idSubasta=-1;
+	if (isset($_GET['id'])){
+		if(is_numeric($_GET['id'])){
+			$sql = "SELECT `titulo` FROM `subastas` WHERE `idSubasta` = ".$_GET['id'] ;
+			$resultado= mysqli_query($conexion, $sql);
+			if( mysqli_num_rows($resultado) == 1){
+				$fila = mysqli_fetch_assoc($resultado);
+				$tituloSubasta= $fila["titulo"];
+				$idSubasta=$_GET['id'];
+			}
+		}
+	}
+	?>
+		<title>BESTNID: <?php echo ($idSubasta== -1? 'Subasta no encontrada':$tituloSubasta); ?></title>
+		<link type="text/css" id="global-css" rel="stylesheet" href="css/header.css" media="all">
+		<link type="text/css" id="global-css" rel="stylesheet" href="css/home.css">
+		<link type="text/css" id="global-css" rel="stylesheet" href="css/detalles.css" media="all">
+		<link rel="icon" type="image/png" href="imagenes/favicon.png">
+		<script src="scripts/countdown.js"></script>
+	</head>
+
+
+	<body>
+		<div class="wrapper">
+			<div class="pagina">
+				<?php
+					include('header.php');
+				?>
+
+				<div class="info-subasta">
+				<?php
+					if($idSubasta == -1){
+						echo '<h2>Subasta no encontrada</h2>';
+					}else{
+						get_info_producto($idSubasta);
+					}
+				?>
+				</div>
+				<div class="preguntas-subasta">
+					<span>Preguntas y Respuestas:</span><br>
+				<?php
+					if($idSubasta != -1){
+						get_PreguntasYRespuestas_producto($idSubasta);
+					}
+				?>
+				</div>
+			</div>	
+		</div>
+	</body>	
+</html>
--- web/modulos/funciones_detalle.php
+++ web/modulos/funciones_detalle.php
@@ -85,12 +85,13 @@ function semi_detalle($idSubasta){
 	$terminada=($fila['fechaFin'])>$fila['now'];
 	echo'
 		<div class="semi-detalle-container">
-			<a href="detalles?id='.$fila['idSubasta'].'">
+			<a href="detalles.php?id='.$fila['idSubasta'].'">
 				<div class="semi-detalle-imagen"><img src="'. $fila['imagen'].'"></div>
 				<div class="semi-detalle-titulo"><span>'.$fila['titulo'].'</span></div>
 				<div class="semi-detalle-countdown">'.($terminada?countdown(strtotime($fila['fechaFin'])):'<p>SUBASTA TERMINADA</p>').'</div>
 			</a>
-		</div>';
+		</div>
+		';
 }
 
 function countdown($time){
@@ -106,5 +107,42 @@ function countdown($time){
 		</script>	
 		';
 	return $contador;
+}
+
+function get_info_producto($idSubasta){
+	include("/modulos/conexion.php");
+	$sql = " SELECT `idSubasta`,`fechaInicio`,`fechaFin`,`titulo`,`descripcion`,`imagen`,`idCategoria`,`idUsuario`, Now() AS now FROM `subastas` WHERE `idSubasta` =".$idSubasta;
+	//$sql = "SELECT `idSubasta`,`fechaInicio`,`fechaFin`,`titulo`,`imagen`, `subastas`.`idCategoria`, `categorias`.`nombre`, `nombreApellido` , Now() AS now FROM `subastas` INNER JOIN `usuarios` ON `subastas`.`idUsuario` = `usuarios`.`idUsuario` INNER JOIN `categorias`ON `categorias`.`idCategoria` = `subastas`.`idCategoria` WHERE `subastas`.`idSubasta` =".$idSubasta;
+	$resultado= mysqli_query($conexion, $sql);
+	$fila = mysqli_fetch_assoc($resultado);
+	$sqlUser = "SELECT `nombreApellido` FROM `usuarios` WHERE `idUsuario` = " .$fila["idUsuario"];
+	$nombreUsuario= mysqli_fetch_assoc( mysqli_query($conexion, $sqlUser))["nombreApellido"];
+	$sqlCategoria="SELECT * FROM `categorias` WHERE `idCategoria` = ". $fila["idCategoria"];
+	$resultado= mysqli_query($conexion, $sqlCategoria);
+	$categoria= mysqli_fetch_assoc($resultado);
+	$terminada=($fila['fechaFin'])>$fila['now'];
+	echo'
+		<h1 class="titulo">'.$fila["titulo"].'</h1>
+		<img src="'. $fila['imagen'].'">
+		<div class="info-lateral">
+			<span>Subastador:</span> '.$nombreUsuario.'<br>
+			<span>Fecha publicaci&oacute;n: </span> '.$fila["fechaInicio"].'<br>
+			<span>Fecha cierre de subasta: </span> '.$fila["fechaFin"].'<br>
+			<span>Tiempo restante de subasta: </span> <br>
+			<div class="contenedor-countdown"><div class="detalle-countdown">'.($terminada?countdown(strtotime($fila['fechaFin'])):'<p>SUBASTA TERMINADA</p>').'</div></div>
+			'.($terminada?'<form><input class="buttom" type="button" href="#" value="Ofertar" ></input></form><br>':'').'
+			<br>
+			<span>Categor&iacute;a:</span> <a class="link-categoria" style="float:none;" href="index.php?categoria='.$categoria["idCategoria"].'" >'.$categoria["nombre"].'</a><br>
+		</div>
+		<div class="info-descripcion">
+			<span>Descripci&oacute;n:</span><br>
+			<p>
+				'.$fila["descripcion"].'
+			</p>
+		</div>';
+}
+function get_PreguntasYRespuestas_producto($idSubasta){
+	//implementar mas tarde
+	echo 'PROXIMAMENTE';
 }
 ?>
