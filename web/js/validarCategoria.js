8253ea7222831d777aeaf7961473f72ed6cfee8f
Mauroech <shukaku-5kaku@hotmail.com>
2015-06-01 07:09:32 -0300

    modificando funcion de validar categoria

--- web/js/validarCategoria.js
+++ web/js/validarCategoria.js
@@ -1,11 +1,28 @@
+String.prototype.trim = function(){ return this.replace(/^\s+|\s+$/g,''); }
+
 function validarCategoria(formCategoria){
-	var nombreCategoria = document.getElementById(formCategoria).value;
+	var nombreCategoria = document.getElementById(formCategoria).value.trim();
+	var soloLetras = /^[a-zA-Z]*(\s[a-zA-Z]*){0,1}$/;
+	 //palabras = nombreCategoria.split(" ");
 	
 	if(nombreCategoria.length < 2 || /^\s+$/.test(nombreCategoria)){
-		alert("El nombre de la categoría contener minímo 3 carácteres");
+		alert("El nombre de la categoría debe contener minímo 3 carácteres");
 		return false;
 	
 	}
+	else if (/^[a-zA-Z]\s.*$/.test(nombreCategoria) ){
+			alert("la primer palabra debe contener al menos 2 caracteres");
+			return false;
+		}
+
 	
+	/*else if(palabras.length > 2) {
+		alert("El nombre de la categoria no puede superar las 2 palabras o mal uso de espacios en blanco");
+		return false;
+	}*/
+	else if(!(soloLetras.test(nombreCategoria))){
+		alert("funcionaaaaaa");
+		return false;
+	}
 	else{return true};
 }
