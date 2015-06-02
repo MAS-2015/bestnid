cedb2deda95b9ad6325a141f36b0728a8ce65429
santicordoba <sdcsantiago@gmail.com>
2015-06-02 17:20:40 -0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	web/formRegistro.php
    	web/imagenes/Thumbs.db
    	web/modulos/check_tel.php
    	web/modulos/login.php

--- web/formRegistro.php
+++ web/formRegistro.php
@@ -7,7 +7,7 @@
 	<input required="required" id="pass" type="password" maxlength="12" name="pass" placeholder="Contrase&ntilde;a"><span class="oblig">*</span><div id="Info4"></div><br>
 		<input required="required" type="password" id="passconf" maxlength="12" name="passconfirmacion" placeholder="Vuelve a ingresar su contrase&ntilde;a"><span class="oblig">*</span><div id="Info5"></div><br>
 		<input required="required" id="tarj" type="text" placeholder="Numero de Tarjeta" name="nroTarj"  maxlength="16" onKeyPress="return soloNumeros(event)"><span class="oblig">*</span><div id="Info2"></div><br>
-	<input type="text" onpaste="return false"  onKeyPress="return soloNumeros(event)" id="tel" placeholder="Numero de Telefono" name="nroTel"><div id="Info6"></div><br>
+	<input type="text" onpaste="return false" maxlength="13" onKeyPress="return soloNumeros(event)" id="tel" placeholder="Numero de Telefono" name="nroTel"><div id="Info6"></div><br>
 	<input type="text" id="domicilio" placeholder="Domicilio" name="domicilio"><div id="Info7"></div><br><br>
 	<input type="submit" id="enviar" value="Registrarse" class="buttom">
 	</form>
