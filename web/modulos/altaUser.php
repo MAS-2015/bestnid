b0de4e55cec354de15963ae6ec828641827bda98
Agustín <agustinlizarralde@gmail.com>
2015-06-26 20:17:25 -0300

    guardar la fecha de registro al registrarse

--- web/modulos/altaUser.php
+++ web/modulos/altaUser.php
@@ -6,7 +6,7 @@
 	$nroTarj= ($_POST["nroTarj"]);
 	$nroTel= addslashes($_POST["nroTel"]);
 	$dom= addslashes($_POST["domicilio"]);
-	$sql = "INSERT INTO usuarios (idUsuario, email, password, domicilio, nombreApellido, nroTarjeta, telefono) VALUES  (NULL,'".$email."','".$pass."','".$dom."','".$nom."','".$nroTarj."', '".$nroTel."')";
+	$sql = "INSERT INTO usuarios (idUsuario, email, password, domicilio, nombreApellido, nroTarjeta, telefono, fechaRegistro) VALUES  (NULL,'".$email."','".$pass."','".$dom."','".$nom."','".$nroTarj."', '".$nroTel."', NOW())";
 	$resultado= mysqli_query($conexion, $sql);
 	mysqli_close($conexion);
  	$msj="La cuenta se creo con exito";
