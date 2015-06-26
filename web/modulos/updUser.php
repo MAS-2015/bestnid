d1814eecb59bcec29152d10db7535ba4b904002e
santicordoba <sdcsantiago@gmail.com>
2015-06-26 01:20:31 -0300

    Correccion modificar datos usuario

--- web/modulos/updUser.php
+++ web/modulos/updUser.php
@@ -1,13 +1,15 @@
 <?php
 	$id=$_POST['id'];
+	echo"$id";
 	$nom=$_POST['nomyApe'];
 	$tarj=$_POST['nroTarj'];
 	$tel=$_POST['nroTel'];
+	echo "$tel";
 	$dom=$_POST['domicilio'];
 	include("conexion.php");
-	$sql="UPDATE usuarios SET nombreApellido=$nom, nroTarjeta=$tarj, nroTelefono=$tel, domicilio=$dom WHERE idUsuario=$id";
+	$sql="UPDATE usuarios SET nombreApellido='".$nom."', nroTarjeta=$tarj, 	telefono=$tel, domicilio='".$dom."' WHERE idUsuario=$id";
 	$resultado = mysqli_query($conexion, $sql);
 	mysqli_close($conexion);
 	$msj='Datos modificados correctamente';
-	header('Location: ../perfil.php?msj='.$msj.'');		
+	
 ?>
