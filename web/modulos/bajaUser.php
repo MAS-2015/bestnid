e20e4891029b1d59e5c5a30937889e2450c1fc9f
Agustín <agustinlizarralde@gmail.com>
2015-06-27 00:08:14 -0300

    mejorado el chequeo de que no tenga pendientes
    falta mas testeo

--- web/modulos/bajaUser.php
+++ web/modulos/bajaUser.php
@@ -2,13 +2,26 @@
 	$id=$_POST['id'];
 	$pass="UsUaRiOdEsAcTiVaDo";
 	include("conexion.php");
-	$sql="UPDATE usuarios SET password='".$pass."' WHERE idUsuario=$id";
-	$resultado = mysqli_query($conexion, $sql);
-	mysqli_close($conexion);
-	session_start();
-	$_SESSION = array();
-	session_destroy();
-	$msj='Cuenta desactivada correctamente';
+	$sql = "SELECT * FROM `ofertas` INJECT INTO `subastas` ON `ofertas`.`idUsuario` = `subastas`.`idUsuario` WHERE `subastas`.`fechaFin` > NOW()  AND `ofertas`.`idUsuario`= '".$id."'";
+	$resultado= mysqli_query($conexion, $sql);
+	$cantOfertas = mysqli_num_rows($resultado);
+	
+	$sql = "SELECT * FROM `subastas` WHERE `idUsuario` = '".$id."' AND `fechaFin` > NOW() ";
+	$resultado= mysqli_query($conexion, $sql);
+	$cantSubastas = mysqli_num_rows($resultado);
+	
+	if( ($cantOfertas + $cantSubastas) == 0){
+		$sql="UPDATE usuarios SET password='".$pass."' WHERE idUsuario=$id";
+		$resultado = mysqli_query($conexion, $sql);
+		mysqli_close($conexion);
+		session_start();
+		$_SESSION = array();
+		session_destroy();
+		$msj='Cuenta desactivada correctamente';
+	}
+	else{
+		$msj='No se puede eliminar la cuenta, hay subastas u ofertas pendientes';
+	}
 	header('Location: ../index.php?msj='.$msj.'');	
 	
 ?>
